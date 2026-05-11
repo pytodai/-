683901894dafd5197a88bf656860fff4ec1b3494
ArtemSokolovg <artem@sokolov.me>
2026-05-11 17:38:29 +0300

    Initial Commit

---
+++ Свободен/________App.swift
@@ -0,0 +1,17 @@
+//
+//  ________App.swift
+//  Свободен
+//
+//  Created by Artem Sokolov on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ СвободенTests/________Tests.swift
@@ -0,0 +1,19 @@
+//
+//  ________Tests.swift
+//  СвободенTests
+//
+//  Created by Artem Sokolov on 5/11/26.
+//
+
+import Testing
+@testable import ________
+
+struct ________Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
